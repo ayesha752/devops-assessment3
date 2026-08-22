c9edbebaafb037b70bfe4b6d79cecf0b27e455fb
ayesha752 <developereign777@gmail.com>
2026-08-23 00:59:00 +0500

    Update Docker setup and application configuration

--- init_sql_scripts/init.sql
+++ init_sql_scripts/init.sql
@@ -4,6 +4,6 @@ create table movie_hero (
 );
 
 insert into movie_hero(movie, hero) values ('Titanic', 'Leonardo DiCaprio');
-insert into movie_hero(movie, hero) values ('The GodFather', 'Marlon Brando');
+insert into movie_hero(movie, hero) values ('The Gambler', 'Marlon Brando');
 insert into movie_hero(movie, hero) values ('Inception', 'Leonardo DiCaprio');
 insert into movie_hero(movie, hero) values ('Russia', 'Moscow');
